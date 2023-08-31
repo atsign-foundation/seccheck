0a728199a7a5ae6e47c364be545807abf43d2122
Colin Constable <colin@constable.cc>
2023-08-31 14:12:06 -0700

    feat: improved the noise factor so it does not drive folks too nuts

--- rootcheck.sh
+++ rootcheck.sh
@@ -4,6 +4,8 @@
 # for example the URL of the GChat webhook to send alerts to
 FULL_PATH_TO_SCRIPT="$(realpath "$0")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
+ALERTS="0"
+MAXCOUNT="3"
 
 if [ ! -f "$SCRIPT_DIRECTORY/.ENV" ]; then
     echo "$SCRIPT_DIRECTORY/.ENV does not exist."
@@ -18,8 +20,23 @@ do
 ./rootcheck.expect
 if (( $? != 0 ))
     then
-    echo "Broke it"
-     curl --location --request POST "${URL}" --header 'Content-Type: application/json' --data-raw "{\"text\": \" Root Server Issue root.atsign.org:64 did not respond from $LOCATION\"}"
+    echo "Root Server not reponding correctly"
+     curl --location --request POST "${URL}" --header 'Content-Type: application/json' --data-raw "{\"text\": \" Root Server Issue root.atsign.org:64 did not respond correctly from $LOCATION\"}"
+     ALERTS=$(( $ALERTS + 1 ))
+
+            if (( $ALERTS >= $MAXCOUNT ))
+            then
+            curl --location --request POST "${URL}" --header 'Content-Type: application/json' --data-raw "{\"text\": \" Root Server Issue repeated consecutively $MAXCOUNT times from $LOCATION sleeping 10 minutes\"}"
+            # Sleep 10 minutes to save the noise
+            sleep 600
+            ALERTS="0"
+            # Notify of resumption of service
+            curl --location --request POST "${URL}" --header 'Content-Type: application/json' --data-raw "{\"text\": \" Root Server checker from $LOCATION is resuming\"}"
+            fi
+    else
+
+ALERTS="0"
 fi
+# sleep as not to hammer root too much
 sleep 3
 done
